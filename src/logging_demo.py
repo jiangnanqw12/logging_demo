e71f62d1c761cd4121bd0d901136548c6dcd94a2
shade <jiangnanqw12@gmail.com>
2024-06-22 10:56:13 +0800

    modified:   src/logging_demo.py         add desired_level uniform_logging_level

--- src/logging_demo.py
+++ src/logging_demo.py
@@ -5,21 +5,30 @@ import matplotlib.pyplot as plt
 log_to_file = True
 show_plots = True
 img_to_file = False
+uniform_logging_level = True  # Set this to True to use a uniform logging level across handlers
+desired_level = logging.DEBUG  # The uniform logging level if uniform_logging_level is True
+
 # Create a logger
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.DEBUG)  # Set the minimum log level
 
-# Create a console handler and set its level to debug
+# Create a console handler and set its level
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+if uniform_logging_level:
+    ch.setLevel(desired_level)
+else:
+    ch.setLevel(logging.INFO)
 
 # Optionally: Create a file handler if log_to_file is True
 if log_to_file:
     fh = logging.FileHandler('logfile.log')
-    fh.setLevel(logging.DEBUG)
+    if uniform_logging_level:
+        fh.setLevel(desired_level)
+    else:
+        fh.setLevel(logging.DEBUG)
     logger.addHandler(fh)
 
-# Create a log format and add it to the console handler
+# Create a log format and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
@@ -32,7 +41,7 @@ plt.figure()
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.title('Test Plot')
 
-
+# Show plot or save to file based on control variables
 if show_plots:
     plt.show()
 if img_to_file:
