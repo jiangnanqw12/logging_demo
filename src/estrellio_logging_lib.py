e2033159ec4d80b51cd82ee13c3372373a99b5ff
shade <jiangnanqw12@gmail.com>
2024-06-27 11:36:15 +0800

    modified:   src/estrellio_logging_lib.py EstrellioLogger get_log_to_file set_log_to_file get_show_plots set_show_plots get_img_to_file set_img_to_file get_uniform_logging_level set_uniform_logging_level get_desired_level set_desired_level get_logger_level set_logger_level get_ch_level set_ch_level get_fh_level set_fh_level get_log_file_name set_log_file_name

--- src/estrellio_logging_lib.py
+++ src/estrellio_logging_lib.py
@@ -1,8 +1,7 @@
-
 import logging
 
 class EstrellioLogger:
-    def __init__(self, log_to_file=True, show_plots=True, img_to_file=False, uniform_logging_level=True, desired_level=logging.DEBUG,logger_level=logging.DEBUG,ch_level=logging.INFO,fh_level=logging.DEBUG,log_file_name="logfile.log"):
+    def __init__(self, log_to_file=True, show_plots=True, img_to_file=False, uniform_logging_level=True, desired_level=logging.DEBUG, logger_level=logging.DEBUG, ch_level=logging.INFO, fh_level=logging.DEBUG, log_file_name="logfile.log"):
         """
         Initialize the logger with specified settings.
 
@@ -13,32 +12,85 @@ class EstrellioLogger:
         - uniform_logging_level (bool): When True, applies a uniform logging level across all handlers.
         - desired_level (logging level): The logging level applied if uniform_logging_level is True.
         """
-        self.log_to_file = log_to_file
-        self.show_plots = show_plots
-        self.img_to_file = img_to_file
-        self.uniform_logging_level = uniform_logging_level
-        self.desired_level = desired_level
+        self._log_to_file = log_to_file
+        self._show_plots = show_plots
+        self._img_to_file = img_to_file
+        self._uniform_logging_level = uniform_logging_level
+        self._desired_level = desired_level
         
-        self.logger_level=logger_level
-        self.ch_level=ch_level
-        self.fh_level=fh_level
-        self.log_file_name=log_file_name
+        self._logger_level = logger_level
+        self._ch_level = ch_level
+        self._fh_level = fh_level
+        self._log_file_name = log_file_name
         self.logger = self.setup_logger()
 
+    def get_log_to_file(self):
+        return self._log_to_file
+
+    def set_log_to_file(self, value):
+        self._log_to_file = value
+
+    def get_show_plots(self):
+        return self._show_plots
+
+    def set_show_plots(self, value):
+        self._show_plots = value
+
+    def get_img_to_file(self):
+        return self._img_to_file
+
+    def set_img_to_file(self, value):
+        self._img_to_file = value
+
+    def get_uniform_logging_level(self):
+        return self._uniform_logging_level
+
+    def set_uniform_logging_level(self, value):
+        self._uniform_logging_level = value
+
+    def get_desired_level(self):
+        return self._desired_level
+
+    def set_desired_level(self, value):
+        self._desired_level = value
+
+    def get_logger_level(self):
+        return self._logger_level
+
+    def set_logger_level(self, value):
+        self._logger_level = value
+
+    def get_ch_level(self):
+        return self._ch_level
+
+    def set_ch_level(self, value):
+        self._ch_level = value
+
+    def get_fh_level(self):
+        return self._fh_level
+
+    def set_fh_level(self, value):
+        self._fh_level = value
+
+    def get_log_file_name(self):
+        return self._log_file_name
+
+    def set_log_file_name(self, value):
+        self._log_file_name = value
+
     def setup_logger(self):
-        """Setup and return a configured logger."""
         logger = logging.getLogger('estrellio_logger')
-        logger.setLevel(self.desired_level if self.uniform_logging_level else self.logger_level)
+        logger.setLevel(self._desired_level if self._uniform_logging_level else self._logger_level)
 
         ch = logging.StreamHandler()
-        ch.setLevel(self.desired_level if self.uniform_logging_level else self.ch_level)
+        ch.setLevel(self._desired_level if self._uniform_logging_level else self._ch_level)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
         logger.addHandler(ch)
 
-        if self.log_to_file:
-            fh = logging.FileHandler(self.log_file_name)
-            fh.setLevel(self.desired_level if self.uniform_logging_level else self.fh_level)
+        if self._log_to_file:
+            fh = logging.FileHandler(self._log_file_name)
+            fh.setLevel(self._desired_level if self._uniform_logging_level else self._fh_level)
             fh.setFormatter(formatter)
             logger.addHandler(fh)
 
@@ -53,7 +105,6 @@ class EstrellioLogger:
     #     elif level.lower() == 'error':
     #         self.logger.error(message)
     def log_message(self, level, message):
-        """Log a message at the given level."""
         getattr(self.logger, level.lower(), self.logger.error)(message)
         'todo, what is getattr'
 def main():
@@ -62,6 +113,8 @@ def main():
     import matplotlib.pyplot as plt
     logger = EstrellioLogger(log_to_file=True, show_plots=True, img_to_file=False, uniform_logging_level=True, desired_level=logging.DEBUG,logger_level=logging.DEBUG,ch_level=logging.INFO,fh_level=logging.DEBUG,log_file_name="logfile.log")
     logger = EstrellioLogger()
+    logger.set_log_to_file(True)
+    logger.set_show_plots(True)
     logger.log_message('info', 'This is an info message.')
     if logger.show_plots or logger.img_to_file:
         data=[1, 2, 3, 4]
